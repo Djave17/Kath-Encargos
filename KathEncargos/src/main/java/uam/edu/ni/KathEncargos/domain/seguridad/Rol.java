79dba9332291d8b9b419c9d2cd1c73b33f0d70aa
David Sánchez <164054263+Djave17@users.noreply.github.com>
2025-11-15 17:57:35 -0600

    Seguridad, Usuarios FROM Franco

--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/seguridad/Rol.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/seguridad/Rol.java
@@ -1,4 +1,35 @@
 package uam.edu.ni.KathEncargos.domain.seguridad;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+import org.openxava.annotations.Required;
+
+import uam.edu.ni.KathEncargos.domain.seguridad.Usuario;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "roles" )
+@Getter @Setter
 
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long id_rol;
+
+    @Column(length = 50, nullable = false)
+    private String nombre_rol;
+
+    @Column(length = 150)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "rol") // Navegación hacia los usuarios
+    private List<Usuario> usuarios;
+
+
+
 }
--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/seguridad/Usuario.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/seguridad/Usuario.java
@@ -1,4 +1,60 @@
 package uam.edu.ni.KathEncargos.domain.seguridad;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+import org.openxava.annotations.ReferenceView;
+import org.openxava.annotations.Required;
+import java.util.Date;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import uam.edu.ni.KathEncargos.domain.seguridad.Rol;
+
+@Entity
+@Table(name = "Usuario")
+@Getter @Setter
+@View(members =
+        "Datos Personales[" +
+                "   nombreUsuario; telefono; email; direccionReferencia;" +
+                "]" +
+                "Seguridad[" +
+                "   contrasena; rol; activo;" +
+                "]" +
+                "Registro[" +
+                "   fechaRegistro;" +
+                "]")
 public class Usuario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id_usuario;
+
+    @Column(length = 50, nullable = false)
+    private String nombre_usuario;
+
+    @Column(length = 20)
+    private String telefono;
+
+    @Column(length = 100, nullable = false)
+    @Stereotype("PASSWORD")
+    private String contraseña;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 150)
+    private String direccion_referencia;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha_registro;
+
+    private Boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name = "Rol_id_rol")
+    @DescriptionsList(descriptionProperties = "nombre")
+    private Rol rol;
 }
